2d80388d76d2d278d465beea11a31d394c587ebc
Astitva Mishra <mailastitva15@gmail.com>
2025-11-13 00:13:06 +0530

    feat: Implement user authentication context and integrate axios for API calls

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import { useContext, useState } from "react";
 import { LayoutTemplate, Menu, X } from "lucide-react";
 import { landingPageStyles } from "../assets/dummystyle";
+import { UserContext } from "../context/UserContext";
+import { ProfileInfoCard } from "../components/Cards";
 
 const LandingPage = () => {
+  const { user } = useContext(UserContext)
+  const [ openAuthModal, setOpenAuthModal ]= useState(false)
+
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className={landingPageStyles.container}>
-        
+    <div className={landingPageStyles.container}>    
       {/* HEADER SECTION */}
       <header className={landingPageStyles.header}>
         <div className={landingPageStyles.headerContainer}>
@@ -15,24 +19,33 @@ const LandingPage = () => {
             <div className={landingPageStyles.logoIcon}>
               <LayoutTemplate className={landingPageStyles.logoIconInner} />
             </div>
-            <span className={landingPageStyles.logoText}>ResumeXpert</span>
+            <span className={landingPageStyles.logoText}>
+              ResumeXpert
+            </span>
           </div>
 
           {/*Mobile Menu Button*/}
-          <button
-            className={landingPageStyles.mobileMenuButton}
-            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-          >
-            {mobileMenuOpen ? (
-              <X size={24} className={landingPageStyles.mobileMenuIcon} />
-            ) : (
-              <Menu size={24} className={landingPageStyles.mobileMenuIcon} />
-            )}
+          <button className={landingPageStyles.mobileMenuButton}
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+            {mobileMenuOpen ? 
+              <X size={24} className={landingPageStyles.mobileMenuIcon} />: 
+              <Menu size={24} className={landingPageStyles.mobileMenuIcon} />}
           </button>
+
+          {/*Desktop Navigation*/}
+          <div className='hidden md:flex items-center'>
+            {user ? (
+              <ProfileInfoCard/>
+            ) : (
+              <button className={landingPageStyles.desktopAuthButton} onClick={() => setOpenAuthModal(true)}>
+                <div className = {landingPageStyles.desktopAuthButtonOverlay}></div>
+                <span className={landingPageStyles.desktopAuthButtonText}>Get Started</span>
+                </button>
+            )}
+          </div>
         </div>
       </header>
     </div>
-  );
+  )
 };
-
 export default LandingPage;
